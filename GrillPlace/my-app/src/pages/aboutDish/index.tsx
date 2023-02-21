fac24c9bc9997a16df8947e9c9835df5784de19f
AnastasiaVasilets <108403908+AnastasiaVasilets@users.noreply.github.com>
2023-02-21 18:50:06 +0300

    Update index.tsx

--- GrillPlace/my-app/src/pages/aboutDish/index.tsx
+++ GrillPlace/my-app/src/pages/aboutDish/index.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { useParams } from "react-router-dom"; //вытаскивает id из url и делает перерисовку
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { addItem ,CartItem } from "redux/slices/cartSlice";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 import styles from './aboutDish.module.css';
 
 const typeNames = ['eat here', 'take away'];
 
+
 const AboutDish: React.FC = () => {
     const [dish, setDish] = React.useState<{
+        id: string,
         img: string;
         title: string;
         price: number;
@@ -18,6 +23,7 @@ const AboutDish: React.FC = () => {
         types: [number];
         souce: [string]
     }>({
+        id: '',
         img: '',
         title: '',
         price: 0,
@@ -28,7 +34,28 @@ const AboutDish: React.FC = () => {
         souce: ['']
     });
     const { id } = useParams();
+    const dispatch = useDispatch();
+    const cartItem = useSelector((state:any) => state.cart.items.find((obj:any) => obj.id === id))
+    const [activeType, setActiveType] = React.useState(0);
+    const [activeSouce, setActiveSouce] = React.useState(0);
+    const addCount = cartItem ? cartItem.count : 0;
 
+    const onClickAdd = () => {
+        const item: CartItem = {
+            id: dish.id,
+            title: dish.title,
+            price: dish.price,
+            img: dish.img,
+            type: typeNames[activeType],
+            souce: dish.souce[activeSouce],
+            count: 0
+        };
+        dispatch(addItem(item))
+    }
+
+    
+
+    //получаем данные о блюде по его id
     React.useEffect(() => {
         async function fetchDish() {
             try {
@@ -42,10 +69,11 @@ const AboutDish: React.FC = () => {
     
     }, [])
 
+    //если блюдо не получено
     if (!dish) {
         return <></>
     }
-    
+    //если блюдо успешно получено
     return (
         <div className={styles.wrap}>
             <h2 className={styles.title}>{dish.title}</h2>
@@ -57,33 +85,41 @@ const AboutDish: React.FC = () => {
                     <p className={styles.about}>{dish.about}</p>
                     <ul className={styles.delete}>Remove from your dish: 
                         {
-                            dish.delete.map((product) => (
+                            dish.delete.map((product) => ( // перебор и вывод элементов массива продуктов, которые можно исключить из блюда
                                 <li>{product}</li>
                             ))
                         }
                     </ul>
                     <ul className={styles.add}>Add to your dish: 
                         {
-                            dish.add.map((product) => (
+                            dish.add.map((product) => ( // перебор и вывод элементов массива продуктов, которые можно добавить в блюдо
                                 <li>{product}</li>
                             ))
                         }
                     </ul>
-                    {/* <div className={styles.dish_block__selector}>
+                    <div className={styles.dish_block__selector}>
                     <ul>
                         {
                         dish.types.map((typeId) => (
-                            <li>{typeNames[typeId]}</li>
+                            <li 
+                            key={typeId}
+                            onClick = {() => setActiveType(typeId)} 
+                            className={activeType === typeId ? styles.active : ''}>
+                                {typeNames[typeId]}</li>
                             ))
                         }
                     </ul>
                     <ul>
                         {
-                        dish.souce.map((souce) => (
-                            <li>{souce} souce</li>))
+                        dish.souce.map((souce, i) => (
+                            <li
+                            key={souce}
+                            onClick = {() => setActiveSouce(i)} 
+                            className={activeSouce === i ? styles.active : ''}>
+                                {souce} souce</li>))
                         }
                     </ul>
-                    </div> */}
+                    </div>
                 </div>
             </div>
             <div className={styles.down}>
@@ -93,11 +129,12 @@ const AboutDish: React.FC = () => {
                         &lt; Back
                     </button>
                     </Link>
-                    {/* <button 
+                    <button 
                             className={styles.add_btn}
+                            onClick={onClickAdd}
                             >
-                            Add
-                        </button> */}
+                            Add {addCount > 0 && <b>{addCount}</b>}
+                        </button>
                 </div>
             </div>
         </div>
